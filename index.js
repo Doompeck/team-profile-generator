37b6743d0f3e2d206abd591ed1016d077ad6f803
Benjamin <bstanf@uw.edu>
2022-08-01 18:29:57 -0700

    Added in pseudo-code

---
+++ index.js
@@ -0,0 +1,29 @@
+// require inquirer
+
+// create an empty array list to store employee objects
+
+// gather manager data
+
+// inquirer prompt
+
+// then build a manager object
+
+// gather engineer data
+
+// inquirer promopt
+
+// then build an engineer object
+
+// gather intern data
+
+// inquirer prompt
+
+// then build an intern object
+
+// ask for which team meber they want to add or are the done
+
+// inquirer prompt
+
+// then deciding which function to call
+
+// generate the html and write it to a file (This should be built in a separate file)
